a2259458fe7224833ec5db76fd0bf897591c0663
kalanguinho222 <miguel.fonseca.goncalves982@gmail.com>
2026-06-11 11:31:56 -0300

    tessste

---
+++ aulas/aula03/ex01_3.js
@@ -0,0 +1,12 @@
+const aluno ={
+    nome: "miguel",
+    idade: 16,
+    cidade: "Cascavel",
+    curso: "D.S"
+};
+
+console.log(`O aluno ${aluno.nome}, de ${aluno.idade} anos, da cidade de ${aluno.cidade} cursa ${aluno.curso} no CEEP`)
+
+aluno.cidade= "cascavel";
+
+console.log(`O aluno ${aluno.nome}, de ${aluno.idade} anos, da cidade de ${aluno.cidade} cursa ${aluno.curso} no CEEP`)
